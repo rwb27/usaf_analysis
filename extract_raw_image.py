92c32c5d62510b8949906649a5b85de17839f03a
Richard Bowman <richard.bowman@cantab.net>
2017-12-19 10:09:05 +0000

    Added usage message & padded TIFF
    TIFF now uses the top, rather than the bottom, 10 bits of the 16-bit file format.  This should make the images easier to view in some viewers.  I've also added a usage message to the script.

--- extract_raw_image.py
+++ extract_raw_image.py
@@ -44,9 +44,18 @@ def extract_file(filename):
         f.write(exif_data_as_string(jpeg))
     
     # extract raw bayer data
-    cv2.imwrite(root_fname + "_raw16.tif", bayer_array.demosaic())
+    cv2.imwrite(root_fname + "_raw16.tif", bayer_array.demosaic()*64)
     cv2.imwrite(root_fname + "_raw8.png", (bayer_array.demosaic()//4).astype(np.uint8))
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: {} <filename.jpg> ...".format(sys.argv[0]))
+        print("Specify one or more filenames corresponding to Raspberry Pi JPEGs including raw Bayer data.")
+        print("Each file will be processed, to produce three new files:")
+        print("<filename>_raw16.tif will contain the full raw data as a 16-bit TIFF file (the lower 6 bits are empty).")
+        print("<filename>_raw8.png will contain the top 8 bits of the raw data, in an easier-to-handle file.")
+        print("<filename>_exif.txt will contain the EXIF metadata extracted as a text file - this includes analogue gain.")
+        sys.exit(0)
+        
     for filename in sys.argv[1:]:
         extract_file(filename)
